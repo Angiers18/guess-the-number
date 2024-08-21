3fa5e07ffea24fa9886dc468fa3d4b483bf3c183
Angie <angiesamper18@gmail.com>
2024-08-20 23:00:05 -0400

    docs: docstrings #7

--- src/user_turn.py
+++ src/user_turn.py
@@ -1,8 +1,12 @@
 def user_turn(): 
     while True:  
-        #captura numero de usuario   
+        """
+        funcion que permite al usuario inroducir sus suposiciones 
+        bucle que permite que se ingresen solo numeros enteros
+        
+        """   
+        
         input_number = input("Introduce un número: ")  
-
         try: 
             user_number = int(input_number)  
             return user_number 
@@ -13,6 +17,13 @@ def user_turn():
             
 
 def validate_number(number): 
+
+    """
+    funcion valida que el numero ingresado este  
+    dentro de los parametros establecidos
+        
+    """ 
+
     if number < 1 or number > 100:  
         print("Ingresa un número valido") 
         user_turn()
